256a9a17c8738d3dfb3a098a43e0f125a3987600
Tim Kuijpers <33220871+TJMKuijpers@users.noreply.github.com>
2022-02-18 15:21:25 +0100

    Add files via upload
    function to create dictionary from input

---
+++ DublinCore/CreateDictionary.py
@@ -0,0 +1,20 @@
+def create_dictionary(title_input,creator_input,subject_input,description_input,publisher_input,contributor_input,date_input,type_input,format_input,identifier_input,source_input,language_input,relation_input,coverage_input,rights_input):
+    # create a dictionary with the input
+    dict_dc={
+    'title':title_input,
+    'creater':creator_input,
+    'subject':subject_input,
+    'description':description_input,
+    'publisher':publisher_input,
+    'contributor':contributor_input,
+    'date':date_input,
+    'type':type_input,
+    'format':format_input,
+    'identifier':identifier_input,
+    'source':source_input,
+    'language':language_input,
+    'relation':relation_input,
+    'coverage':coverage_input,
+    'rights':rights_input
+    }
+    return dict_dc
